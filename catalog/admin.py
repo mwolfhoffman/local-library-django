8ae03bebca0b8e7eec41770e2a0e0f04012c7f5c
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2023-01-03 09:44:57 -0700

    admin updates

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,52 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
+    # fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+    fieldsets = (
+        (None, {
+            'fields':  ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
+        }),
+        # ('Title', {
+        #     'fields': ('title', 'author')
+        # }),
+    )
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
