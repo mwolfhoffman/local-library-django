7235cf3bc149b41e365f50c2124328dec5e56735
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2023-01-06 08:50:32 -0700

    book list and detail pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 
@@ -11,7 +12,8 @@ def index(request):
     num_genres = Genre.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
@@ -21,8 +23,30 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_genres' : num_genres
+        'num_genres': num_genres
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+def books(request):
+    """View function for books list page of site."""
+
+    books = Book.objects.all()
+    print(books)
+    context = {
+        'book_list': books,
+    }
+
+    return render(request, 'book_list.html', context=context)
+
+
+def book_detail(request, pk):
+    """View function for book detail page of site."""
+    book = Book.objects.get(id=pk)
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'book_detail.html', context=context)
