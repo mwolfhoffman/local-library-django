0a10812484954f3b72df03e69bfcb04041443752
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2023-01-06 08:04:58 -0700

    index page

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
