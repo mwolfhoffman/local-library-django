b569b15697ad28cd91dfada1cb73c395c88a94df
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2023-01-10 15:41:18 -0700

    renew page and form

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('authors/<int:pk>', views.author_detail, name='author-detail'),
     path('mybooks/', views.my_books, name='my-borrowed'),
     path('borrowed/', views.all_borrowed, name='all-borrowed'),
-
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian,
+         name='renew-book-librarian'),
 ]
